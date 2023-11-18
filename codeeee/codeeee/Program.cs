0466f9705f4e5530762cd072d88e7c060587470e
Sollimba <mrjesus759@gmail.com>
2023-11-18 03:28:41 +0300

    Новая ветка

---
+++ codeeee/codeeee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        User user = new User("John", 24, "john.doe@example.com");
+
+        Console.WriteLine("Name: " + user.Name);
+        Console.WriteLine("Age: " + user.Age);
+        Console.WriteLine("Email: " + user.Email);
+    }
+}
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+
+    public User(string name, int age, string email)
+    {
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+}
